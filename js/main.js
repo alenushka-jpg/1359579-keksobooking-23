1119b9fcb59ba88f5b588607d059a68e94b1f9b9
Alyona <alionxlo@gmail.com>
2021-06-02 18:59:54 +0300

    {

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function randomNumber(min, max) {
   // получить случайное число от (min-0.5) до (max+0.5)
-  let number = min - 0.5 + Math.random() * (max - min + 1);
+  const number = min - 0.5 + Math.random() * (max - min + 1);
   return (number);
 }
 
