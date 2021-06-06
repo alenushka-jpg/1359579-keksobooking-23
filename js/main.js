831dd6913b4483b382e55da2c3226b7a0a9d309d
Alyona <alionxlo@gmail.com>
2021-06-06 21:39:10 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-function randomNumber(min, max) {
+/**
+ * Генерация случайного числа из диапазона
+ * @param {Number} min - минимальное значение
+ * @param {Number} max - максимальное значение
+ * @return {Number} - случайное число
+ */
+
+function randomNumber(min, max, num) {
   // получить случайное число от (min-0.5) до (max+0.5)
-  const number = min - 0.5 + Math.random() * (max - min + 1);
-  return (number);
+  const number = min - 0.5 + num + Math.random() * (max - min + 1 + num);
+
+  num = 1.22;
+  num.toFixed(3);
+
+  if (min > max) {
+    [min, max] = [max, min];
+  } return Math.random(number);
 }
 
-randomNumber(0.5, 1);
+randomNumber(0.5, 1.5);
