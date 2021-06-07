5f8e831f1e76363c6d05ca15bd9db3b41bd596c3
Alyona <alionxlo@gmail.com>
2021-06-07 17:43:33 +0300

    Вторая функция

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@
  * Генерация случайного числа из диапазона
  * @param {Number} min - минимальное значение
  * @param {Number} max - максимальное значение
+ *  @param {Number} num - количество знаков после запятой
  * @return {Number} - случайное число
  */
 
 function randomNumber(min, max, num) {
   // получить случайное число от (min-0.5) до (max+0.5)
-  const number = min - 0.5 + num + Math.random() * (max - min + 1 + num);
+  const number = min - 0.5 + Math.random() * (max - min + 1);
 
   num = 1.22;
   num.toFixed(3);
@@ -18,3 +19,22 @@ function randomNumber(min, max, num) {
 }
 
 randomNumber(0.5, 1.5);
+
+/**
+ * Генерация случайного целого числа из диапазона
+ * @param {Number} min - минимальное значение
+ * @param {Number} max - максимальное значение
+ * @return {Number} - случайное целое число
+ */
+
+function randomInteger(min, max) {
+  // случайное число от min до (max+1)
+  const rand = min + Math.random() * (max + 1 - min);
+
+
+  if (min > max) {
+    [min, max] = [max, min];
+  } return Math.floor(rand);
+}
+
+randomInteger(1, 5);
