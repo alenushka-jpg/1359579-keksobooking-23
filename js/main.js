735251f465a163cdbc028bbcedb574281a65b089
Alyona <alionxlo@gmail.com>
2021-06-08 21:00:31 +0300

    module2-task1

--- js/main.js
+++ js/main.js
@@ -2,39 +2,25 @@
  * Генерация случайного числа из диапазона
  * @param {Number} min - минимальное значение
  * @param {Number} max - максимальное значение
- *  @param {Number} num - количество знаков после запятой
+ * @param {Number} num - количеством знаков после запятой
  * @return {Number} - случайное число
  */
-
-function randomNumber(min, max, num) {
+function getRandomNumber(min, max, num = 0) {
+  if (min < 0 || max < 0) {
+    return 'Числа не могут быть отрицательными';
+  }
+  if (min > 0) {
+    [min, max] = [max, min];
+  }
   // получить случайное число от (min-0.5) до (max+0.5)
-  const number = min - 0.5 + Math.random() * (max - min + 1);
-
-  num = 1.22;
-  num.toFixed(3);
-
-  if (min > max) {
-    [min, max] = [max, min];
-  } return Math.random(number);
+  const number = +(min - 0.5 + Math.random() * (max - min + 1)).toFixed(num);
+  return(number);
 }
 
-randomNumber(0.5, 1.5);
+getRandomNumber(0.5, 1.5);
 
-/**
- * Генерация случайного целого числа из диапазона
- * @param {Number} min - минимальное значение
- * @param {Number} max - максимальное значение
- * @return {Number} - случайное целое число
- */
-
-function randomInteger(min, max) {
-  // случайное число от min до (max+1)
-  const rand = min + Math.random() * (max + 1 - min);
-
-
-  if (min > max) {
-    [min, max] = [max, min];
-  } return Math.floor(rand);
+function getRandomInteger() {
+  return getRandomNumber;
 }
 
-randomInteger(1, 5);
+getRandomInteger(1, 5);
