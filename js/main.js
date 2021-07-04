6a4f13a26f87122f2efe10f472a4ba13268a979c
alenushka <alionxlo@gmail.com>
2021-07-04 16:12:23 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// import {getRandomNumber} from './util.js';
-// import {generateAdverts} from './util.js';
-// import {getRandomElement} from './util.js';
-// import {requestArray} from './data.js';
 import {generateAdvert} from './data.js';
 
 generateAdvert(6);
