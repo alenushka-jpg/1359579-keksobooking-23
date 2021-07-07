a34a2914311bb465e5a3d5273ca8b31201c62b32
alenushka <alionxlo@gmail.com>
2021-07-07 17:09:29 +0300

    Сделано неактивное состоянии  страницы .1

--- js/form.js
+++ js/form.js
@@ -1,10 +1,17 @@
 const adForm = document.querySelector('.ad-form');
-const adFormHeader = document.querySelectorAll('.ad-form-header');
+const adFormElement = document.querySelectorAll('.ad-form__element');
 const mapFilters = document.querySelector('.map__filters');
 const mapFeatures = document.querySelectorAll('.map__features');
-
+const elementsDisabled = [adFormElement, mapFeatures];
 
 adForm.classList.toggle('ad-form--disabled');
 mapFilters.classList.toggle('map__filters--disabled');
-adFormHeader.attr('disabled');
-mapFeatures.attr('disabled');
+
+elementsDisabled.forEach((el) => {
+  el.setAttribute('disabled', 'disabled');
+});
+
+const map = L.map('map', {
+  center: [56.4444, 56.4444],
+  zoom: 15,
+});
